a8724c77ddc6f4ed562a96389135c3208ae23c38
James Hardy <jamesthardy@gmail.com>
2019-05-24 18:02:24 +0100

    react hook basics

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-    render() {
+const App = props => {
+    const [ personsState, setPersonsState ] = useState({
+        persons: [
+            {name: 'James', age: 30},
+            {name: 'Liz', age: 30},
+            {name: 'Lappy', age: 1}
+        ]
+    });
+
+const[otherState, setOtherState] = useState('some other value');
+
+console.log(personsState , otherState);
+    
+    const switchNameHandler = () => {
+        // console.log('was clicked!');
+        setPersonsState({
+            persons: [
+            {name: 'James', age: 100},
+            {name: 'Liz', age: 99},
+            {name: 'Lappy', age: 62}
+        ],
+        otherState: personsState.otherState
+    });
+    };
         return (
             <div className="App">
                 <h1>Hi, I'm a react app!!</h1>
-                <Person />
-                <Person />
-                <Person />
+                <button onClick={switchNameHandler}>Switch Name</button>
+                <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+                <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+                <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
             </div>
         );
-        // return React.createElement('div' , {className: 'App'} , React.createElement('h1' , null , 'does this work now?'));
     }
-}
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const person = () => {
-     return <p>I'm a person and I am {Math.floor(Math.random() * 30)} years old!</p>
+// the argument props contains an object with all of the proporties specified in the App.js file
+//  
+
+const person = (props) => {
+     return (
+     <div>
+         <p>I'm {props.name} and I am {props.age} years old!</p>
+         <p>{props.children}</p> 
+     </div>
+     )
  }
 
 
